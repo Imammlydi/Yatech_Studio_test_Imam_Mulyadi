1add35688d77d8064b501d0611dcea920abfde09
Imammlydi <57788518+Imammlydi@users.noreply.github.com>
2024-04-30 10:09:34 +0700

    Update Logic.js

--- Soal_Nomer_1 (Logic)/Logic.js
+++ Soal_Nomer_1 (Logic)/Logic.js
@@ -1,22 +1,20 @@
-
 function kategoriNilai(nilai) {
-    if (nilai = 86 && nilai = 100) {
+    if (nilai >= 86 && nilai <= 100) {
         return 'A';
-    } else if (nilai = 71 && nilai = 85) {
+    } else if (nilai >= 71 && nilai <= 85) {
         return 'B';
-    } else if (nilai = 56 && nilai = 70) {
+    } else if (nilai >= 56 && nilai <= 70) {
         return 'C';
-    } else if (nilai = 41 && nilai = 55) {
+    } else if (nilai >= 41 && nilai <= 55) {
         return 'D';
-    } else if (nilai = 0 && nilai = 40) {
+    } else if (nilai >= 0 && nilai <= 40) {
         return 'E';
     } else {
         return 'Nilai tidak valid';
     }
 }
 
-
-  penggunaan fungsi
+// Contoh penggunaan fungsi:
 const nilai = 75;
 const kategori = kategoriNilai(nilai);
-console.log(Kategori nilai, kategori);
+console.log("Kategori nilai:", kategori);
